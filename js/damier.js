cc57c63f772ee284335a2428894c5132f7a721bc
p16016063 <jeremie.passerat@etu.univ-amu.fr>
2018-02-13 16:51:57 +0100

    Cela n'est point fonctionnel :(

--- js/damier.js
+++ js/damier.js
@@ -10,31 +10,60 @@ let Damier;
         this.dest=dest;
         this.joueurencours = true;
         this.get_caractere = function () {
-            console.log(this.joueurencours);
+         //   console.log(this.joueurencours);
             if (this.joueurencours)
                 return 'X';
             else
                 return 'O';
         };
+
+        this.t= [];
+        this.tab = [this.t];
+        this.ligne_faite = function () {
+
+            for (let i = 0; i < 3; ++i) {
+                var j = 0;
+                    /*if (self.tab[i][j] === self.tab[i][j + 1] && self.tab[i][j + 1]
+                        === self.tab[i][j + 2] && self.tab[i][j + 2] !== undefined) {
+                        console.log('a' + self.tab[i][j]);
+                        console.log('a' + self.tab[i][j + 1]);
+                        console.log('a' + self.tab[i][j + 2]);
+                    }
+                   else*/ if (self.tab[j][i] === self.tab[j + 1][i] && self.tab[j + 1][i]
+                        === self.tab[j + 2][i] && self.tab[j + 2][i] !== undefined) {
+                    console.log('b' + self.tab[0][i]);
+                    console.log('b' + self.tab[1][i]);
+                    console.log('b' + self.tab[2][i]);
+                }
+                console.log('b' + self.tab[j+1][i]);
+
+            }
+            return false;
+        };
+
+
+
+
         this.td_click = function () {
             let i = $(this).data('i');
             let j = $(this).data('j');
             $(this).html(self.get_caractere());
+            self.tab[i][j] = self.joueurencours;
             self.joueurencours = !self.joueurencours;
-            console.log('test');
+            if (self.ligne_faite())
+                console.log("Match fini");
+           // else
+                self.tab[i][j] = !self.joueurencours;
 
-            //self.tab[i] = !self.joueurencours;
-
-         //   console.log(self.tab[i]);
         };
 
-        let t= [];
-        let tab = [t];
+
         let damier = $(dest);
         let self = this;
         for (let i = 0; i < long; ++i) {
 
             let tr = $('<tr />');
+            self.t.push();
 
             for (let j = 0; j < larg; ++j) {
 
@@ -55,6 +84,8 @@ let Damier;
                             .click(self.td_click)
                     );
             }
+
+            self.tab.push(self.t);
 
             damier.append(tr);
         }
