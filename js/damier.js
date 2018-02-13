4f39a215030ca7301620ad0937033a20208225c8
p16016063 <jeremie.passerat@etu.univ-amu.fr>
2018-02-13 17:21:09 +0100

    Cela n'est tjrs point fonctionnel :(

--- js/damier.js
+++ js/damier.js
@@ -23,20 +23,20 @@ let Damier;
 
             for (let i = 0; i < 3; ++i) {
                 var j = 0;
-                    /*if (self.tab[i][j] === self.tab[i][j + 1] && self.tab[i][j + 1]
+                    if (self.tab[i][j] === self.tab[i][j + 1] && self.tab[i][j + 1]
                         === self.tab[i][j + 2] && self.tab[i][j + 2] !== undefined) {
                         console.log('a' + self.tab[i][j]);
                         console.log('a' + self.tab[i][j + 1]);
                         console.log('a' + self.tab[i][j + 2]);
                     }
-                   else*/ if (self.tab[j][i] === self.tab[j + 1][i] && self.tab[j + 1][i]
-                        === self.tab[j + 2][i] && self.tab[j + 2][i] !== undefined) {
+                   else if (self.tab[0][i] === self.tab[1][i]){//} && self.tab[j + 1][i]
+                  //      === self.tab[j + 2][i] && self.tab[j + 2][i] !== undefined) {
                     console.log('b' + self.tab[0][i]);
                     console.log('b' + self.tab[1][i]);
                     console.log('b' + self.tab[2][i]);
+                    return true
                 }
                 console.log('b' + self.tab[j+1][i]);
-
             }
             return false;
         };
@@ -48,13 +48,12 @@ let Damier;
             let i = $(this).data('i');
             let j = $(this).data('j');
             $(this).html(self.get_caractere());
+            console.log (i, j);
             self.tab[i][j] = self.joueurencours;
-            self.joueurencours = !self.joueurencours;
             if (self.ligne_faite())
-                console.log("Match fini");
-           // else
-                self.tab[i][j] = !self.joueurencours;
-
+                alert("Vous avez gagné");
+            else
+                self.joueurencours = !self.joueurencours;
         };
 
 
